741439c1b7388854ccb75489dcb844638cb3fe46
Gabriela Prestes Farias <gabiprestesfarias@gmail.com>
2024-06-14 15:35:17 -0300

    add new file

---
+++ codigos/introducao-python/operadores.py
@@ -0,0 +1,39 @@
+# > OPERAÇÕES MATEMÁTICAS (ARITMÉTICAS)
+
+# - Soma: +
+# - Subtração: -
+# - Multiplicação: *
+# - Divisão: /
+# - Divisão inteira: // (descarta a parte decimal)
+# - Resto da divisão: % (operador módulo)
+# - Potência: **
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)  # Qual é o resto da divisão de 20 por 3
+print(2 ** 3)
+
+# > OPERADORES BOOLEANOS
+
+# Realizando comparações:
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print('Python' == 'python')
+# == usado para verificar se dois valores são iguais
+print('banana' != 'abacaxi')
+# != signifca "diferente de"
+print(altura1 >= altura2)
+print(altura2 > altura3)
